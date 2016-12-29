628e84c3d4b2bc51b632cbc0f7d50fe58df68e86
Brian Mont <briansmont830@gmail.com>
2016-12-29 16:02:01 +0000

    commit with skeleton of TodoApp.jsx

--- app/app.jsx
+++ app/app.jsx
@@ -3,15 +3,16 @@ var ReactDOM = require('react-dom');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 var $ = require('jQuery');
 
-// load foundation
+var TodoApp = require('TodoApp');
 
-//$(document).foundation();
+// load foundation
+// $(document).foundation();
 
 // load APP Css
 require('style!css!sass!applicationStyles')
 
 
 ReactDOM.render(
-  <p>Boilerplate code, for router, check out timer app or weather app!</p>,
+  <TodoApp/>,
   document.getElementById('app')
 );
---
+++ app/components/TodoApp.jsx
@@ -0,0 +1,14 @@
+var React = require('react');
+
+
+var TodoApp = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <p>TodoApp will live here!</p>
+      </div>
+    );
+  }
+});
+
+module.exports = TodoApp;
