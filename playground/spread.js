76231b15b17f32bd637cff7297e13e5d84a223ee
Brian Mont <briansmont830@gmail.com>
2016-12-30 15:06:07 +0000

    spread operator basics

---
+++ playground/spread.js
@@ -0,0 +1,15 @@
+// function add(a,b) {
+//   return a + b;
+// }
+
+// console.log(add(3,1));
+
+// var toAdd = [9,5];
+// console.log(add(...toAdd));
+
+var groupA = ['brian', 'kirstin'];
+var groupB = ['benny'];
+
+var final = [3, ...groupA];
+
+console.log(final);
