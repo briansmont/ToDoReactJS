81a0098b405f49d5340461f20450c9f364a53bbf
Brian Mont <briansmont830@gmail.com>
2017-01-05 19:04:24 +0000

    refactor tests to work with new store

--- app/components/Todo.jsx
+++ app/components/Todo.jsx
@@ -1,9 +1,11 @@
 var React = require('react');
+var {connect} = require('react-redux');
 var moment = require('moment');
+var actions = require('actions');
 
-var Todo = React.createClass({
+export var Todo = React.createClass({
   render: function() {
-    var {text, id, completed, createdAt, completedAt} = this.props;
+    var {text, id, completed, createdAt, completedAt, dispatch} = this.props;
     var todoClassName = completed ? 'todo todo-completed' : 'todo';
     
     var renderDate = () => {
@@ -20,7 +22,8 @@ var Todo = React.createClass({
     
     return (
       <div className={todoClassName} onClick={() => {
-        this.props.onToggle(id);
+        // this.props.onToggle(id);
+        dispatch(actions.toggleTodo(id));
       }}>
         <div>
           <input type="checkbox" checked={completed}/>
@@ -35,4 +38,5 @@ var Todo = React.createClass({
   }
 });
 
-module.exports = Todo;
+export default connect()(Todo);
+
