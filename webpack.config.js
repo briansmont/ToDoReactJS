ca2d8851c65845179261bcb88d40db1067b5857a
Brian Mont <briansmont830@gmail.com>
2017-01-30 22:23:52 +0000

    refactor webpack and package.json to minimize deploy size

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@
 var webpack = require('webpack');
 var path = require('path');
 
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 module.exports = {
   entry: [
@@ -24,7 +25,12 @@ module.exports = {
     new webpack.ProvidePlugin({
       '$': 'jquery',
       'jQuery': 'jquery',
-    })
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        warnings: false,
+      }
+    }),
   ],
   
   resolve: {
@@ -62,5 +68,5 @@ module.exports = {
       path.resolve(__dirname, './node_modules/foundation-sites/scss')
     ]
   },
-  devtool: 'cheap-module-eval-source-map'
+  devtool: process.env.NODE_ENV === 'production' ? undefined : 'cheap-module-eval-source-map'
 };
