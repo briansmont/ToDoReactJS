b43eb10583504e331c79cbe8d630e45fa3764afe
Brian Mont <briansmont830@gmail.com>
2017-01-30 17:44:00 +0000

    refactored so localstorage no longer saves and presents todos to view, now they are parsed from information on firebase

--- app/tests/api/TodoAPI.test.jsx
+++ app/tests/api/TodoAPI.test.jsx
@@ -12,48 +12,6 @@ describe('TodoAPI', () => {
   it('should exist', () => {
     expect(TodoAPI).toExist();
   });
-  
-  describe('setTodos', () => {
-    it('should set valid todos array', () => {
-      var todos = [{
-        id: 1,
-        text: 'sample task',
-        completed: false,
-      }];
-      
-      TodoAPI.setTodos(todos);
-      var actualTodos = JSON.parse(localStorage.getItem('todos'));
-      
-      expect(actualTodos).toEqual(todos);
-    });
-    it('should not set INVALID todos array', () => {
-      var badTodos = {value: 'bad information'};
-      TodoAPI.setTodos(badTodos);
-      
-      expect(localStorage.getItem('todos')).toBe(null);
-    });
-  });
-  
-  describe('getTodos', () => {
-    it('should return an empty array when there is bad/no data', () => {
-      var actualTodos = TodoAPI.getTodos();
-      expect(actualTodos).toEqual([]);
-    });
-    
-    it('should return todo if valid array exists in the localStorage', () => {
-      var todos = [{
-        id: 1,
-        text: 'sample task',
-        completed: false,
-      }];
-      localStorage.setItem('todos', JSON.stringify(todos));
-      
-      var actualTodos = TodoAPI.getTodos();
-      
-      expect(actualTodos).toEqual(todos);
-    });
-  });
-  
   describe('filterTodos', () => {
     var todos = [{
       id: 1,
